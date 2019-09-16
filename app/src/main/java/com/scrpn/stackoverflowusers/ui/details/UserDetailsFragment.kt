4799d8c960d02aa8d2a33f7a723025f077e4113f
Marton Varga <scorpeeon@gmail.com>
2019-09-17 00:23:00 +0100

    Implementing details layout (WIP)

--- app/src/main/java/com/scrpn/stackoverflowusers/ui/details/UserDetailsFragment.kt
+++ app/src/main/java/com/scrpn/stackoverflowusers/ui/details/UserDetailsFragment.kt
@@ -3,11 +3,13 @@ package com.scrpn.stackoverflowusers.ui.details
 import android.os.Bundle
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import com.bumptech.glide.Glide
 import com.scrpn.stackoverflowusers.AppComponent
 import com.scrpn.stackoverflowusers.R
 import com.scrpn.stackoverflowusers.domain.model.User
 import com.scrpn.stackoverflowusers.ui.BaseFragment
 import kotlinx.android.synthetic.main.fragment_details.*
+import kotlinx.android.synthetic.main.item_detail.*
 import javax.inject.Inject
 
 class UserDetailsFragment: BaseFragment(), UserDetailsScreen {
@@ -49,6 +51,17 @@ class UserDetailsFragment: BaseFragment(), UserDetailsScreen {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         setupToolbar()
+        initializeLayout(user)
+    }
+
+    private fun initializeLayout(user: User?) {
+        toolbarLayout.title = getString(R.string.app_name)
+        name.text = getString(R.string.name, user?.displayName)
+        userId.text = getString(R.string.user_id, user?.userId.toString())
+        reputation.text = getString(R.string.reputation, user?.reputation.toString())
+        location.text = getString(R.string.location, user?.location)
+        creationDate.text = getString(R.string.creation_date, user?.creationDate.toString()) // TODO
+        Glide.with(requireContext()).load(user?.profileImage).into(toolbarImage)
     }
 
     private fun setupToolbar() {
